74b0c5a9d9cdf866a435a2e1d19dcb8ca1cc1044
tonyJSH <90615742+tonyJSH@users.noreply.github.com>
2024-04-10 18:30:13 +0900

    Add files via upload

---
+++ model_conv.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO('yolov8n.pt')
+
+# Export the model to TensorRT format
+model.export(format='engine')  # format='engine'으로 지정하면 TensorRT 사용
+
+# Load the exported TensorRT model
+tensorrt_model = YOLO('yolov8n.engine')
+
+# Run inference
+results = tensorrt_model('https://ultralytics.com/images/bus.jpg')
