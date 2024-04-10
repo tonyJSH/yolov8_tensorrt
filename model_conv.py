ca4cd3b17fd9d26c82768d5367a38883fce33f79
tonyJSH <90615742+tonyJSH@users.noreply.github.com>
2024-04-10 19:35:16 +0900

    [fix] yolov8n -> yolov8-pose

--- model_conv.py
+++ model_conv.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8n-pose.pt')
 
 # Export the model to TensorRT format
 model.export(format='engine')  # format='engine'으로 지정하면 TensorRT 사용
 
 # Load the exported TensorRT model
-tensorrt_model = YOLO('yolov8n.engine')
+tensorrt_model = YOLO('yolov8n-pose.engine')
 
 # Run inference
 results = tensorrt_model('https://ultralytics.com/images/bus.jpg')
